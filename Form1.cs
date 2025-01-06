f53fbaa3fe2fb116a380580a8442fa60a5da0921
HaPhuVinh <hpv2404@gmail.com>
2025-01-07 01:17:08 +0700

    Get_TOP_Info()

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,8 +22,17 @@ namespace Bearing_Enhancer_CAN
         private void button1_Click(object sender, EventArgs e)
         {
             string PJN = textBox_PJNum.Text;
-            Top_Plate_Info TOP = new Top_Plate_Info();
-            TOP.Get_TOP_Info(PJN);
+            if (File.Exists(@"C:\SST-EA\Client\Projects\" + PJN + @"\Temp\" + PJN + ".txt"))
+            {
+                Top_Plate_Info TOP = new Top_Plate_Info();
+                TOP.Get_TOP_Info(PJN, "A");
+            }
+            else
+            {
+                MessageBox.Show($"Please create {PJN}.txt file!");
+            }
+            
+            
            
         }
 
--- Top_Plate_Info.cs
+++ Top_Plate_Info.cs
@@ -11,38 +11,81 @@ namespace Bearing_Enhancer_CAN
 {
     public class Top_Plate_Info
     {
+        public string JointID { get; set; }
+        public string XLocation { get; set; }
         public double Reaction { get; set; }
-        public string TOPSpecie { get; set; }
+        public string Material { get; set; }
         public string BearingWidth { get; set; }
-        public string InterfaceWidth { get; set; }
         public string RequireWidth { get; set; }
         public string LoadTransfer { get; set; }
 
         public void Get_TOP_Info(string PJN,string trussname)
         {
             string proNumber = PJN;
-            string trussName = trussname;
-            string path = @"";
+            //string trussName = trussname;
+            string tempName="";
+            string path = "";
             List<string> txtFile = new List<string>();
-            string[] arrLine;
-             
+            string[] arrLine = {""};
             path = @"C:\SST-EA\Client\Projects\"+ proNumber + @"\Temp\" + proNumber + ".txt";//Need to see more
-            using (StreamReader reader = new StreamReader(path))
+            string content = File.ReadAllText(path);
+            string[] arrFile = content.Split('\n');
+            int i = 0;
+            bool pickup = false;
+            foreach (string line in arrFile)
             {
-                string line;
-                while ((line=reader.ReadLine()) != null)
+                arrLine = line.Split(':');
+                
+                if (line.Contains("Truss:") && line.Contains("Qty"))
                 {
-                    if (line.Contains("Qty:") && line.Contains("Truss:")&&line.Contains(trussName))
-                    {
-
-                        arrLine = line.Split();
-
-                    }
-                    //txtFile.Add(line);
-
+                    tempName = $"Truss:{arrLine[arrLine.Length - 1]}";
+                    tempName = tempName.TrimEnd('\r');
                 }
+
+                if(line.Contains("Jnt")&& line.Contains("X-Loc") && line.Contains("React") && line.Contains("Up") && line.Contains("Width") && line.Contains("Reqd") && line.Contains("Mat"))
+                {
+                    pickup = true;
+                    continue;
+                }
+
+                if (line.Contains("Max Horiz ="))
+                {
+                    break;
+                }
+
+                if ($"Truss:  {trussname}" == tempName && pickup == true)
+                {
+                    string line2 = line.Trim();
+                    string[] arrLine2 = line2.Split();
+                    Top_Plate_Info tPI = new Top_Plate_Info();
+                    tPI.JointID = arrLine2[0];
+                    tPI.XLocation = arrLine2[1];
+                    tPI.Reaction = double.Parse(arrLine2[2]);
+                    tPI.BearingWidth = arrLine2[3];
+                    tPI.RequireWidth = arrLine2[4];
+                    tPI.Material = arrLine2[5];
+                }
+
+                
+
+                i = i + 1;
             }
-            
+            //using (StreamReader reader = new StreamReader(path))
+            //{
+            //    string line;
+            //    while ((line=reader.ReadLine()) != null)
+            //    {
+            //        if (line.Contains("Truss:")&&line.Contains("Qty"))
+            //        {
+            //            arrLine = line.Split(':');
+            //            string tempName = $"Truss:{arrLine[arrLine.Length-1]}";
+
+            //        }
+
+
+            //    }
+            //}
+
         }
     }
 
