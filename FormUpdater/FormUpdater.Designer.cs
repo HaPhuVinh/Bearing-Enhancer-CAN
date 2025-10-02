5077068d274c128c449cb442901e57111281b7dd
HaPhuVinh <hpv2404@gmail.com>
2025-10-02 18:54:30 +0700

    Added Updater project

---
+++ FormUpdater/FormUpdater.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FormUpdater
+{
+    partial class FormUpdater
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 32);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(351, 22);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(9, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(45, 15);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Label1";
+            // 
+            // FormUpdater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 66);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "FormUpdater";
+            this.Text = "Updater";
+            this.Load += new System.EventHandler(this.FormUpdater_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ FormUpdater/FormUpdater.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FormUpdater
+{
+    public partial class FormUpdater : Form
+    {
+        public FormUpdater()
+        {
+            InitializeComponent();
+        }
+
+        private string zipFile, appDir, mainExe;
+
+        public FormUpdater(string[] args)
+        {
+            InitializeComponent();
+
+            if (args.Length != 3)
+            {
+                MessageBox.Show("Wrong Parameters!");
+                Environment.Exit(1); // Dừng 
+                return;
+            }
+
+            zipFile = args[0];
+            appDir = args[1];
+            mainExe = args[2];
+        }
+
+        private async void FormUpdater_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lblStatus.Text = "Updating...";
+                progressBar1.Style = ProgressBarStyle.Marquee;
+
+                await Task.Run(() =>
+                {
+                    // 1. Xóa file cũ (tùy chọn)
+                    // 2. Giải nén
+                    if (Directory.Exists(appDir))
+                    {
+                        foreach (var file in Directory.GetFiles(appDir))
+                        {
+                            File.Delete(file); // chú ý file đang chạy không được xóa
+                        }
+                    }
+                    
+                    System.IO.Compression.ZipFile.ExtractToDirectory(zipFile, appDir);
+                });
+
+                // 3. Xóa file zip
+                try { File.Delete(zipFile); } catch { }
+
+                lblStatus.Text = "Update completed. Restarting...";
+                progressBar1.Style = ProgressBarStyle.Continuous;
+
+                // 3. Chạy lại ứng dụng chính
+                Process.Start(Path.Combine(appDir, @"Release\Bearing Enhancer CAN.exe"));
+
+                // 4. Thoát
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update Error: " + ex.Message);
+            }
+        }
+    }
+}
